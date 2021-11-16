0b04f9308737dbe0047fcde15e7ca60641b8235c
Kajetan Puchalski <2457183P@student.gla.ac.uk>
2021-11-16 14:47:38 +0000

    Add a useless passing test for CI to work

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,7 @@
-/* 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:roadsage/main.dart';
-*/
-
-void main() {}
+void main() {
+  test('Useless test should pass', () {
+    expect(1 + 1, 2);
+  });
+}
