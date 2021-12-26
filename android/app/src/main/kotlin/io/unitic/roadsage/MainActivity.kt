0b35b25956d3409440312ec4e6c54dc623a22865
Kajetan Puchalski <2457183P@student.gla.ac.uk>
2021-12-26 02:40:55 +0000

    Intent logging within MainActivity.kt
    Include automatic logging of received intent extras in the onCreate() method
    of MainActivity. Useful for Google Assistant app actions testing.
    
    Signed-off-by: Kajetan Puchalski <2457183P@student.gla.ac.uk>

--- android/app/src/main/kotlin/io/unitic/roadsage/MainActivity.kt
+++ android/app/src/main/kotlin/io/unitic/roadsage/MainActivity.kt
@@ -1,5 +1,8 @@
 package io.unitic.roadsage
 
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
@@ -7,6 +10,25 @@ import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "siri_suggestions"
+    private val TAG = "RoadSageMainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        logIntent(intent)
+    }
+
+    private fun logIntent(intent: Intent) {
+        val bundle: Bundle = intent.extras ?: return
+
+        Log.d(TAG, "======= logIntent ========= %s")
+        Log.d(TAG, "Logging intent data start")
+
+        bundle.keySet().forEach { key ->
+            Log.d(TAG, "[$key=${bundle.get(key)}]");
+        }
+
+        Log.d(TAG, "Logging intent data complete")
+    }
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
