0ca5b0bb46488d643f9d81c5ec506d392c0ea0df
Kajetan Puchalski <2457183P@student.gla.ac.uk>
2022-01-19 00:47:10 +0000

    Add BLE support library and basic device scanning
    This commit lays the groundwork for in-app Bluetooth.
    
    - Add flutter_blue library for Bluetooth support
    - Add Android permissions for Bluetooth access to manifest.xml
    - Bump minSdkVersion to 19 (necessary for Bluetooth)
    - Show a toast with discovered Bluetooth devices
    
    Solves issue #29
    
    Signed-off-by: Kajetan Puchalski <2457183P@student.gla.ac.uk>

--- lib/screens/devices.dart
+++ lib/screens/devices.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:roadsage/constants.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
 class DevicesScreen extends StatefulWidget {
   const DevicesScreen({Key? key}) : super(key: key);
@@ -9,6 +11,33 @@ class DevicesScreen extends StatefulWidget {
 }
 
 class _DevicesScreenState extends State<DevicesScreen> {
+  @override
+  void initState() {
+    FlutterBlue flutterBlue = FlutterBlue.instance;
+
+    flutterBlue.startScan(timeout: Duration(seconds: 4));
+
+    Fluttertoast.showToast(msg: "Scanning for Bluetooth devices..");
+    // Listen to scan results
+    var subscription = flutterBlue.scanResults.listen((results) {
+      // do something with scan results
+      print('listening');
+      for (ScanResult r in results) {
+        print('${r.device.name} found! rssi: ${r.rssi}');
+        Fluttertoast.showToast(msg: "${r.device.name} found!");
+      }
+    });
+
+    subscription.onDone(() {
+      print('listening done');
+    });
+
+    // Stop scanning
+    flutterBlue.stopScan();
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
