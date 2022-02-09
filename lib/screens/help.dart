42dac4354cf8b19e2cc4e9ab0908f1161d74f9a2
Fatma <2515006a@student.gla.ac.uk>
2022-02-09 21:26:32 +0530

    Fixed the issue to convert the help and feed back screen by using a flutter form with validation
    Signed-off-by: Kajetan Puchalski <2457183P@student.gla.ac.uk>

--- lib/screens/help.dart
+++ lib/screens/help.dart
@@ -9,95 +9,121 @@ class HelpScreen extends StatefulWidget {
 }
 
 class _HelpScreenState extends State<HelpScreen> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-          padding: const EdgeInsets.only(top: 28.0),
-          child: ListView(
-              // mainAxisAlignment: MainAxisAlignment.start,
-              // mainAxisSize: MainAxisSize.max,
-              // crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                const Padding(
-                    child: Text(
-                      RoadSageStrings.helpAndFeedbackDesc,
-                      style: Fonts.middleFont,
-                      textAlign: TextAlign.center,
-                    ),
-                    padding: EdgeInsets.all(20.0)),
-
-                // TODO: possibly convert to TextFormFields?
-                Padding(
-                    child: Container(
-                      color: const Color.fromARGB(60, 220, 220, 220),
-                      child: const TextField(
+    return Form(
+      key: _formKey,
+      child: Container(
+        child: Padding(
+            padding: const EdgeInsets.only(top: 28.0),
+            child: ListView(
+                // mainAxisAlignment: MainAxisAlignment.start,
+                // mainAxisSize: MainAxisSize.max,
+                // crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  const Padding(
+                      child: Text(
+                        RoadSageStrings.helpAndFeedbackDesc,
                         style: Fonts.middleFont,
-                        maxLines: 1,
-                        decoration: InputDecoration(
-                            hintText: RoadSageStrings.formName,
-                            filled: true,
-                            fillColor: Colors.white),
+                        textAlign: TextAlign.center,
                       ),
-                    ),
-                    padding: const EdgeInsets.fromLTRB(20, 1.0, 20, 10.0)),
-                Padding(
-                    child: Container(
-                      color: const Color.fromARGB(60, 220, 220, 220),
-                      child: const TextField(
-                        style: Fonts.middleFont,
-                        maxLines: 1,
-                        decoration: InputDecoration(
-                            hintText: RoadSageStrings.formEmail,
-                            filled: true,
-                            fillColor: Colors.white),
+                      padding: EdgeInsets.all(20.0)),
+
+                  // TODO: possibly convert to TextFormFields?
+                  Padding(
+                      child: Container(
+                        color: const Color.fromARGB(60, 220, 220, 220),
+                        child: TextFormField(
+                          style: Fonts.middleFont,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your name';
+                            }
+                            return null;
+                          },
+                          maxLines: 1,
+                          decoration: const InputDecoration(
+                              hintText: RoadSageStrings.formName,
+                              filled: true,
+                              fillColor: Colors.white),
+                        ),
                       ),
-                    ),
-                    padding: const EdgeInsets.fromLTRB(20, 1.0, 20, 10.0)),
-
-                const Padding(
-                    child: Text(
-                      RoadSageStrings.giveFeedback,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    padding: EdgeInsets.fromLTRB(20, 1.0, 20, 10.0)),
-
-                Padding(
-                    child: Container(
-                      color: const Color.fromARGB(60, 220, 220, 220),
-                      child: const TextField(
-                        style: Fonts.middleFont,
-                        maxLines: 13,
-                        decoration: InputDecoration(
-                            fillColor: Colors.white,
-                            filled: true,
-                            hintText: RoadSageStrings.feedbackHint),
+                      padding: const EdgeInsets.fromLTRB(20, 1.0, 20, 10.0)),
+                  Padding(
+                      child: Container(
+                        color: const Color.fromARGB(60, 220, 220, 220),
+                        child: TextFormField(
+                          style: Fonts.middleFont,
+                          maxLines: 1,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your email';
+                            } else if (!RegExp(
+                                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                .hasMatch(value)) {
+                              return 'Please enter a valid email';
+                            }
+                            return null;
+                          },
+                          decoration: const InputDecoration(
+                              hintText: RoadSageStrings.formEmail,
+                              filled: true,
+                              fillColor: Colors.white),
+                        ),
                       ),
-                    ),
-                    padding: const EdgeInsets.fromLTRB(20, 1.0, 20, 10.0)),
+                      padding: const EdgeInsets.fromLTRB(20, 1.0, 20, 10.0)),
 
-                Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 30),
-                    child: SizedBox(
-                        width: double.infinity,
-                        height: 50,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              primary: const Color(0xFF0AC0FF),
-                              textStyle: const TextStyle(fontSize: 20)),
-                          onPressed: () => submit(),
-                          child: const Text(RoadSageStrings.formSubmit),
-                        ))),
+                  const Padding(
+                      child: Text(
+                        RoadSageStrings.giveFeedback,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      padding: EdgeInsets.fromLTRB(20, 1.0, 20, 10.0)),
 
-                // Contactus
-                getBtnContainer(RoadSageStrings.contactUs, ""),
-                getBtnContainer(RoadSageStrings.faqs, Routes.faq),
-                getBtnContainer(RoadSageStrings.rateUs, "")
-              ])),
-      color: const Color(0xFFDCDCDC),
-      padding: const EdgeInsets.all(0.0),
-      alignment: Alignment.center,
+                  Padding(
+                      child: Container(
+                        color: const Color.fromARGB(60, 220, 220, 220),
+                        child: TextFormField(
+                          style: Fonts.middleFont,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter some feedback';
+                            }
+                            return null;
+                          },
+                          maxLines: 13,
+                          decoration: const InputDecoration(
+                              fillColor: Colors.white,
+                              filled: true,
+                              hintText: RoadSageStrings.feedbackHint),
+                        ),
+                      ),
+                      padding: const EdgeInsets.fromLTRB(20, 1.0, 20, 10.0)),
+
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 20, 20, 30),
+                      child: SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                primary: const Color(0xFF0AC0FF),
+                                textStyle: const TextStyle(fontSize: 20)),
+                            onPressed: () => submit(_formKey),
+                            child: const Text(RoadSageStrings.formSubmit),
+                          ))),
+
+                  // Contactus
+                  getBtnContainer(RoadSageStrings.contactUs, ""),
+                  getBtnContainer(RoadSageStrings.faqs, Routes.faq),
+                  getBtnContainer(RoadSageStrings.rateUs, "")
+                ])),
+        color: const Color(0xFFDCDCDC),
+        padding: const EdgeInsets.all(0.0),
+        alignment: Alignment.center,
+      ),
     );
   }
 
@@ -119,7 +145,8 @@ class _HelpScreenState extends State<HelpScreen> {
     );
   }
 
-  static void submit() {
-    // TODO: implement callback
+  static void submit(GlobalKey<FormState> formKey) {
+    formKey.currentState!.save();
+    if (formKey.currentState!.validate()) {}
   }
 }
